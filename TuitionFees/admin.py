8fb34e8c96db3e635287524bb01199a637d7b184
Vivek Vijayan (@Official) <vivekvijayan3@icloud.com>
2022-01-07 17:40:24 +0530

    DB created for Fees

---
+++ TuitionFees/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import FeesCollector, Month
+# Register your models here.
+
+admin.site.register(FeesCollector)
+admin.site.register(Month)
---
+++ TuitionFees/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.contrib.auth.models import User
+# Create your models here.
+
+class Month(models.Model):
+    month = models.CharField(primary_key=True, max_length=200)
+
+
+
+class FeesCollector(models.Model):
+    student = models.ForeignKey(User, on_delete=models.PROTECT)
+    fees = models.IntegerField()
+    paid_month = models.ForeignKey(Month, on_delete=models.PROTECT)
+    paid_year = models.IntegerField()
+    late_pay = models.BooleanField(default=False)
+    late_pay_fine = models.IntegerField(default=0)
+    payment_on = models.DateTimeField(auto_now=True)
+    paid_for = models.CharField(max_length=200)
+
+    def __str__(self) -> str:
+        return str(self.student) + " Paid: Rs." + str(int(self.fees) + int(self.late_pay_fine)) + " - Paid on : " + str(self.payment_on) + " - PERIOD : " + str(self.paid_month) + " " + str(self.paid_year) 
