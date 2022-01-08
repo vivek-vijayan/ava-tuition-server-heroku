68d4e8f6fe4f40361cd22ba19ccb182c9876b2f6
Vivek Vijayan (@Official) <vivekvijayan3@icloud.com>
2022-01-09 00:17:54 +0530

    Added PAYMENTMODE

--- TuitionFees/models.py
+++ TuitionFees/models.py
@@ -9,6 +9,13 @@ class Month(models.Model):
         return self.month
 
 class FeesCollector(models.Model):
+
+    PAYMENT_MODE = [
+        ('CASH', 'CASH'),
+        ('UPI', 'UPI'),
+        ('CARD', 'CARD'),
+    ]
+
     student = models.ForeignKey(User, on_delete=models.PROTECT)
     fees = models.IntegerField()
     paid_month = models.ForeignKey(Month, on_delete=models.PROTECT)
@@ -17,6 +24,7 @@ class FeesCollector(models.Model):
     late_pay_fine = models.IntegerField(default=0)
     payment_on = models.DateTimeField(auto_now=True)
     paid_for = models.CharField(max_length=200)
+    payment_mode = models.CharField(max_length=200, choices=PAYMENT_MODE, default="CASH")
 
     def __str__(self) -> str:
         return str(self.student) + " Paid: Rs." + str(int(self.fees) + int(self.late_pay_fine)) + " - PERIOD : " + str(self.paid_month) + " " + str(self.paid_year) 
